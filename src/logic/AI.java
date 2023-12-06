2614bc25c4763e1f3fb3cb3d68ca3b6af354f2b9
leamolina <120037925+leamolina@users.noreply.github.com>
2023-12-06 17:18:11 +0100

    Début du code

--- src/logic/AI.java
+++ src/logic/AI.java
@@ -110,6 +110,103 @@ public class AI{
 	 * @return a string describing the next action (among PacManLauncher.UP/DOWN/LEFT/RIGHT)
 	 */
 	public static String findNextMove(BeliefState beliefState) {
-		return PacManLauncher.LEFT;
+
+		int deepth = 3; //La profondeur de notre recherche
+		//String bestAction = PacManLauncher.LEFT;
+		Object[] decision  = getPotentialScore(deepth, beliefState);
+		int potentialScore = (int) decision[0];
+		String bestAction = String.valueOf(decision[1]);
+
+		/* int bestPotentialScore = 0;
+		Plans plan = beliefState.extendsBeliefState();
+		for(int i=0; i<plan.size(); i++){
+			Result result = plan.getResult(i);
+			//int potentialScore = 0;
+			for(BeliefState beliefState1:result.getBeliefStates()){
+				int potentialScore = getPotentialScore(deepth, beliefState1);
+				//Si elle est meilleure, on récupère son action
+				if(potentialScore>bestPotentialScore){
+					bestPotentialScore = potentialScore;
+					bestAction = plan.getAction(i).get(0);
+				}
+			}
+		}*/
+
+		//CASE:UP
+		//CASE:DOWN
+		//CASE:LEFT
+		//CASE:RIGHT
+
+		return bestAction;
 	}
+
+	// On applique l'algorithme And Or avec une limite de profondeur
+	private static Object[] getPotentialScore(int deepth, BeliefState beliefState) {
+		Object[] decision = new Object[2];
+		//On descend jusqu'à notre limite deepth (en faisait attention à s'arrêter si y'a un état final)
+		if(beliefState.getLife()==0 || beliefState.getNbrOfGommes()==0){
+			return new Object[] {beliefState.getScore()};
+		}
+		else if(deepth == 0) {
+			return getHeuristic(beliefState);
+		}
+		else {
+			int bestScore = 0;
+			String bestAction = PacManLauncher.LEFT;
+			//On passe à la profondeur suivante
+			Plans plan = beliefState.extendsBeliefState();
+			int score = 0;
+			//Je parcours chaque choix
+			for (int i = 0; i < plan.size(); i++) {
+				Result result = plan.getResult(i); //Je le récupère
+
+
+
+				//Je vais parcourir les différents beliefState à l'intérieur de ce result
+				for (BeliefState beliefState1 : result.getBeliefStates()) {
+
+					score = (int) getPotentialScore(deepth - 1, beliefState1)[0];
+					if (score > bestScore) {
+						bestScore = score;
+						bestAction = plan.getAction(i).get(0);
+ 					}
+				}
+
+
+
+
+			}
+			return new Object[] {bestScore,bestAction};
+		}
+
+
+	}
+
+	/*private static String orSearch(BeliefState beliefState){
+		if(beliefState.getLife()==0 || beliefState.getNbrOfGommes()==0){
+			return null;
+		}
+		//Si on l'a déjà rencontré
+		else if{
+			return
+		}
+		else{
+			Plans plans = beliefState.extendsBeliefState();
+			for(int i=0; i<plans.size(); i++){
+				String action = andSearch(plans.getResult(0).)
+			}
+		}
+	}*/
+
+	private static int getHeuristic(BeliefState beliefState) {
+		//Nombre de gommes mangées
+		//Nombre de fantomes en vue / Nombre de fantomes pas en vue
+		//CompteurPeur (en fonction des cases qui te séparent d'un fantôme) --> pas obligatoire
+		//Si le pacman est proche des gommes/supergommes
+		//Score qu'on a déjà
+	}
+
+
+	//Un OU pour chaque mouvement
+	//Un ET pour que element de result
 }
