6d30f489bdfd56bf380d424ce077baf01ef8f95e
leamolina <120037925+leamolina@users.noreply.github.com>
2023-12-13 18:24:14 +0100

    Implémentation de l algo AND / OR & amélioration de l heuristique

--- src/logic/AI.java
+++ src/logic/AI.java
@@ -109,24 +109,48 @@ public class AI{
 	/**
 	 * function that compute the next action to do (among UP, DOWN, LEFT, RIGHT)
 	 * @param beliefState the current belief-state of the agen
-	 * @param deepth the deepth of the search (size of the largest sequence of action checked)
 	 * @return a string describing the next action (among PacManLauncher.UP/DOWN/LEFT/RIGHT)
 	 */
-	public static String findNextMoveBis(BeliefState beliefState) {
+	public static String findNextMove(BeliefState beliefState) {
 		int deepth = 4;
+		ArrayList<String> bestMoves = new ArrayList<>();
 		Plans plan = beliefState.extendsBeliefState();
+		double maxPotentialScore = Double.NEGATIVE_INFINITY;
 		for(int i=0; i<plan.size(); i++) {
 			Result result = plan.getResult(i);
-			//On parcours tous les beliefState de ce result, et on calcule leur heuristique --> On renvoie la moyenne de l'heuristique
-			int potentialScore = getPotentialScore(result,deepth);
+			double potentialScore = getPotentialScore(result,deepth);
+			if(potentialScore>maxPotentialScore){
+				maxPotentialScore = potentialScore;
+				bestMoves.clear();
+				for(int j=0; j<plan.getAction(i).size(); j++){
+					bestMoves.add(plan.getAction(i).get(j));
+				}
+			}
+			else if(potentialScore==maxPotentialScore){
+				for(int j=0; j<plan.getAction(i).size(); j++){
+					bestMoves.add(plan.getAction(i).get(j));
+				}
+			}
 		}
 		//On choisit le max de toutes les moyennes choisies
-		return PacManLauncher.LEFT;
+		if(bestMoves.size()>0){
+			Random random = new Random();
+			int randomNumber = random.nextInt(bestMoves.size());
+			return bestMoves.get(randomNumber);
+		}
+		else{
+			//Pour la dernière itération (avant qu'il se fasse manger, y'a aucun bon score donc listBestMove sera vide)
+			return PacManLauncher.LEFT;
+		}
 	}
 
 	private static double getPotentialScore(Result result, int deepth) {
+		//Si on a terminé le jeu (qu'on a atteint un état final)
+		if(isFinalNextMap(result)){
+			return 1000000+1;
+		}
 		//Si on a terminé de parcourir
-		if(deepth==0){
+		else if(deepth==0){
 			double average=0;
 			for(BeliefState beliefState: result.getBeliefStates()){
 				average+=getHeuristic(beliefState);
@@ -134,355 +158,80 @@ public class AI{
 			return average/result.getBeliefStates().size();
 		}
 		else{
+			double sumScore = 0;
 			for(BeliefState beliefState: result.getBeliefStates()){
 				Plans plan = beliefState.extendsBeliefState();
+				double scoreMax = Double.NEGATIVE_INFINITY;
 				for(int i=0; i<plan.size(); i++) {
 					Result res = plan.getResult(i);
 					double score = getPotentialScore(res, deepth-1);
+					if(score>scoreMax){
+						scoreMax = score;
+					}
 				}
+				sumScore+=scoreMax;
 			}
+			double averageScore = sumScore/result.getBeliefStates().size();
+			return averageScore;
 		}
 
 	}
 
-	public static String findNextMove(BeliefState beliefState) {
-		System.out.println("Nb gommes = " + beliefState.getNbrOfGommes());
-		ArrayList<String> listBestMove = new ArrayList<>();
-		int deepth = 3; //La profondeur de notre recherche
-		double bestScore = Double.NEGATIVE_INFINITY;
-		String bestMove = PacManLauncher.LEFT;
-		//String bestAction = PacManLauncher.LEFT;
-		//Object[] decision  = getPotentialScore(deepth, beliefState);
-		Plans plan = beliefState.extendsBeliefState();
-		for(int i=0; i<plan.size(); i++) {
-			Result result = plan.getResult(i);
-			double sum = 0;
-			int cpt = 0;
-			for(BeliefState beliefState1:result.getBeliefStates()){
-				cpt++;
-				double potentialScore = getHeuristic(beliefState1);
-				System.out.println("Potental score : " + potentialScore);
-				sum+=potentialScore;
+	private static boolean isFinalNextMap(Result result) {
+		for(BeliefState beliefState: result.getBeliefStates()){
+			if(beliefState.getNbrOfGommes()==0){
+				return true;
 			}
-			double average = sum/cpt;
-			System.out.println("L'average est : " + average + " et le move associé est " + plan.getAction(i).get(0));
-			if(average>bestScore){
-				listBestMove.clear();
-				System.out.println("On met à jour notre meilleur score");
-				bestScore = average;
-				for(int j=0; j<plan.getAction(i).size(); j++){
-					listBestMove.add(plan.getAction(i).get(j));
-				}
-
-				/*Random random = new Random();
-				int randomNumber = random.nextInt(plan.getAction(i).size());
-				bestMove = plan.getAction(i).get(randomNumber);*/
-			}
-			else if(average == bestScore){
-				for(int j=0; j<plan.getAction(i).size(); j++){
-					listBestMove.add(plan.getAction(i).get(j));
-				}
-			}
-
 		}
-		/*System.out.println("On a choisi ce move " + bestMove + " car son heuristique est " + bestScore);*/
-		//On return un move au hasard parmis tous les best move
-		if(listBestMove.size()>0){
-			Random random = new Random();
-			int randomNumber = random.nextInt(listBestMove.size());
-			return listBestMove.get(randomNumber);
-		}
-		else{
-			//Pour la dernière itération (avant qu'il se fasse manger, y'a aucun bon score donc listBestMove sera vide)
-			return PacManLauncher.LEFT;
-		}
-
-		/*return bestMove;*/
-
-
-
-
-
-		//String bestAction = String.valueOf(decision[1]);
-
-		/* int bestPotentialScore = 0;
-		Plans plan = beliefState.extendsBeliefState();
-		for(int i=0; i<plan.size(); i++){
-			Result result = plan.getResult(i);
-			//int potentialScore = 0;
-			for(BeliefState beliefState1:result.getBeliefStates()){
-				int potentialScore = getPotentialScore(deepth, beliefState1);
-				//Si elle est meilleure, on récupère son action
-				if(potentialScore>bestPotentialScore){
-					bestPotentialScore = potentialScore;
-					bestAction = plan.getAction(i).get(0);
-				}
-			}
-		}*/
-
-		//CASE:UP
-		//CASE:DOWN
-		//CASE:LEFT
-		//CASE:RIGHT
-
+		return false;
 	}
 
-	// On applique l'algorithme And Or avec une limite de profondeur
-	/*private static Object[] getPotentialScore(int deepth, BeliefState beliefState) {
-		Object[] decision = new Object[2];
-		//On descend jusqu'à notre limite deepth (en faisait attention à s'arrêter si y'a un état final)
-		if(beliefState.getLife()==0 || beliefState.getNbrOfGommes()==0){
-			return new Object[] {beliefState.getScore()};
-		}
-		else if(deepth == 0) {
-			return getHeuristic(beliefState);
-		}
-		else {
-			int bestScore = 0;
-			String bestAction = PacManLauncher.LEFT;
-			//On passe à la profondeur suivante
-			Plans plan = beliefState.extendsBeliefState();
-			int score = 0;
-			//Je parcours chaque choix
-			for (int i = 0; i < plan.size(); i++) {
-				Result result = plan.getResult(i); //Je le récupère
-
-
-
-				//Je vais parcourir les différents beliefState à l'intérieur de ce result
-				for (BeliefState beliefState1 : result.getBeliefStates()) {
-
-					score = (int) getPotentialScore(deepth - 1, beliefState1)[0];
-					if (score > bestScore) {
-						bestScore = score;
-						bestAction = plan.getAction(i).get(0);
- 					}
-				}
-
-
-
-
-			}
-			return new Object[] {bestScore,bestAction};
-		}
-
-
-	}*/
-
-	/*private static String orSearch(BeliefState beliefState){
-		if(beliefState.getLife()==0 || beliefState.getNbrOfGommes()==0){
-			return null;
-		}
-		//Si on l'a déjà rencontré
-		else if{
-			return
-		}
-		else{
-			Plans plans = beliefState.extendsBeliefState();
-			for(int i=0; i<plans.size(); i++){
-				String action = andSearch(plans.getResult(0).)
+	private static boolean isFinalGameOver(Result result) {
+		for(BeliefState beliefState: result.getBeliefStates()){
+			if(beliefState.getLife()==0){
+				return true;
 			}
 		}
-	}*/
+		return false;
+	}
+
+	private static boolean isFinal(Result result) {
+		for(BeliefState beliefState: result.getBeliefStates()){
+			if(beliefState.getLife()==0 || beliefState.getNbrOfGommes()==0){
+				return true;
+			}
+		}
+		return false;
+	}
+
 
 	private static double getHeuristic(BeliefState beliefState) {
-		if(beliefState.getLife()==0){return -1000000;}
+		if(beliefState.getLife()==0){return 0;}
+
 
 		double bonus = 0;
-		double malus = 0;
-
 		int lignePacman = beliefState.getPacmanPosition().getRow();
 		int colonnePacman = beliefState.getPacmanPosition().getColumn();
-		char dirPacman = beliefState.getPacmanPosition().getDirection();
-
+		//char dirPacman = beliefState.getPacmanPosition().getDirection();
 
 		//Le faire aller vers la gomme la plus proche
-		int minDistanceMannhatanGommes = 0;
+		double minDistanceMannhatanGommes = 100000;
 		for (int i=0; i<20 ; i++){ //nb de ligne
 			for(int j=0; j<19 ; j++) { //nb de colonnes
 				if (beliefState.getMap(i, j) == '.') {
-					int distanceMannhatanGommes = Math.abs(lignePacman - i) + Math.abs(colonnePacman - j);
-					if(distanceMannhatanGommes<minDistanceMannhatanGommes){
+					double distanceMannhatanGommes = Math.abs(lignePacman - i) + Math.abs(colonnePacman - j);
+					if(distanceMannhatanGommes < minDistanceMannhatanGommes){
 						minDistanceMannhatanGommes = distanceMannhatanGommes;
 					}
 				}
 			}
 		}
-		malus+=0.01* minDistanceMannhatanGommes;
-
-		//Le faire aller vers la superGomme la plus proche
-		if(beliefState.getNbrOfSuperGommes()>0) {
-			int minDistanceMannhatanSuperGommes = 0;
-			for (int i = 0; i < 20; i++) { //nb de ligne
-				for (int j = 0; j < 19; j++) { //nb de colonnes
-					if (beliefState.getMap(i, j) == '*') {
-						int distanceMannhatanSuperGommes = Math.abs(lignePacman - i) + Math.abs(colonnePacman - j);
-						if (distanceMannhatanSuperGommes < minDistanceMannhatanSuperGommes) {
-							minDistanceMannhatanSuperGommes = distanceMannhatanSuperGommes;
-						}
-					}
-				}
-			}
-			malus += 0.1 * minDistanceMannhatanSuperGommes;
+		if(minDistanceMannhatanGommes!=0){
+			bonus = 1/minDistanceMannhatanGommes;
 		}
-
-		//On parcourt la liste des positions des fantômes potentiels
-		/*for(int i = 0 ; i<3; i++) {
-
-			TreeSet<Position> tree0 = beliefState.getGhostPositions(i);
-			for (Position p : tree0) {
-				int colonneGhost = p.getColumn();
-				int ligneGhost = p.getRow();
-				//On veut qu'il fuie les fantômes
-				if(lignePacman == ligneGhost && !isWallRowBetween(p, beliefState)){
-					int distance = Math.abs(colonneGhost - colonnePacman);
-					malus+=100-distance;
-				}
-				if(colonnePacman == colonneGhost && !isWallColBetween(p, beliefState)){
-					int distance = Math.abs(ligneGhost - lignePacman);
-					malus+=100-distance;
-				}
-
-
-			}
-		}*/
-
-		return beliefState.getScore() + bonus-malus;
+		return beliefState.getScore() + bonus;
 	}
 
-
-
-	private static double getHeuristicBis(BeliefState beliefState) {
-		if(beliefState.getLife()==0){return -1000000;}
-
-		double bonus = 0;
-		double malus = 0;
-
-		int yPacman = beliefState.getPacmanPosition().getRow();
-		int xPacman = beliefState.getPacmanPosition().getColumn();
-		char dirPacman = beliefState.getPacmanPosition().getDirection();
-
-		//On vérifie si à gauche / droite /haut / bas y'a une superGomme ou une gomme
-		/*double cptSuperGomme = 0;
-		double cptGomme = 0;
-		if(beliefState.getMap(xPacman-1, yPacman) =='*'){
-			cptSuperGomme++;
-		}
-		else if(beliefState.getMap(xPacman-1, yPacman) =='.'){
-			cptGomme++;
-		}
-		if(beliefState.getMap(xPacman+1, yPacman) =='*'){
-			cptSuperGomme++;
-		}
-		else if(beliefState.getMap(xPacman+1, yPacman) =='.'){
-			cptGomme++;
-		}
-		if(beliefState.getMap(xPacman, yPacman-1) =='*'){
-			cptSuperGomme++;
-		}
-		else if(beliefState.getMap(xPacman, yPacman-1) =='.'){
-			cptGomme++;
-		}
-		if(beliefState.getMap(xPacman, yPacman+1) =='*'){
-			cptSuperGomme++;
-		}
-		else if(beliefState.getMap(xPacman, yPacman+1) =='.'){
-			cptGomme++;
-		}
-		bonus+=(cptSuperGomme)*100;
-		bonus+=(cptGomme)*10;*/
-
-
-		//Nombre de gommes mangées
-		//Nombre de fantomes en vue / Nombre de fantomes pas en vue
-		//CompteurPeur (en fonction des cases qui te séparent d'un fantôme) --> pas obligatoire
-		//Si le pacman est proche des gommes/supergommes
-		//Score qu'on a déjà
-
-		//Premier fantome
-		int cptGhostSameDirection = 0;
-		int cptGhostPeur = 0;
-		for(int i = 0 ; i<3; i++) {
-
-
-			TreeSet<Position> tree0 = beliefState.getGhostPositions(i);
-			for (Position p : tree0) {
-
-				//Une position potentielle du fantome
-
-				//On vérifie si y'a un fantôme qui nous fonce dessus (qui n'est pas effrayé)
-				if (((dirPacman == 'L' && p.dir == 'R') || (dirPacman == 'R' && p.dir == 'L')) && p.y == yPacman && !isWallRowBetween(p, beliefState) && beliefState.getCompteurPeur(i)<2*Math.abs(xPacman-p.x)+1) {
-					cptGhostSameDirection+=10-Math.abs(xPacman-p.x);
-				}
-				if (((dirPacman == 'U' && p.dir == 'D') || (dirPacman == 'D' && p.dir == 'U')) && p.x == xPacman && !isWallColBetween(p, beliefState) && beliefState.getCompteurPeur(i)<2*Math.abs(p.y-yPacman)+1) {
-					cptGhostSameDirection +=10-Math.abs(p.y - yPacman);
-				}
-				//On va rajouter du bonus s'il y a un fantôme effrayé très proche
-				if (p.y == yPacman && p.x < xPacman && !isWallRowBetween(p, beliefState) && beliefState.getCompteurPeur(i)>=2*Math.abs(xPacman-p.x)+1) {
-					int peur = beliefState.getCompteurPeur(i);
-					cptGhostPeur +=10-Math.abs(xPacman - p.x);
-				}
-				if (p.x == xPacman && p.y > yPacman && !isWallColBetween(p, beliefState) && beliefState.getCompteurPeur(i)>=2*Math.abs(p.y-yPacman)+1) {
-					int peur = beliefState.getCompteurPeur(i);
-					cptGhostPeur+=10-Math.abs(p.y-yPacman);
-				}
-				System.out.println("Cpt ghost peur = " + cptGhostPeur);
-			}
-		}
-		malus += cptGhostSameDirection*10000;
-		bonus += cptGhostPeur*1000;
-
-		//On vérifie la direction des fantômes
-
-		if(beliefState.getNbrOfSuperGommes()>0){
-			//On parcourt le map pour trouver la position d'une superGomme
-			 for (int i=0; i<19 ; i++){
-				 for(int j=0; j<20 ; j++){
-					 if(beliefState.getMap(i,j) == '*'){
-						 //Calcul de la distance de Manhattan qui sépare notre Pacman de la super gomme
-						 int distanceManhatan = Math.abs(xPacman - i) + Math.abs(yPacman - j);
-						 malus +=distanceManhatan*0.1;
-					 }
-				 }
-			 }
-		}
-
-
-
-
-		//Fuir les murs
-		/*double cptMur = 0;
-		if(beliefState.getMap(xPacman-1, yPacman) =='#'){
-			cptMur++;
-		}
-		//On regarde ce qu'il y a droite de pacMan
-		if(beliefState.getMap(xPacman+1, yPacman) =='#'){
-			cptMur++;
-		}
-		if(beliefState.getMap(xPacman, yPacman-1) =='#'){
-			cptMur++;
-		}
-		if(beliefState.getMap(xPacman, yPacman+1) =='#'){
-			cptMur++;
-		}
-
-		malus+=cptMur/4;*/
-
-
-
-		//Il va préférer un état où il y a moins de gommes
-		//malus +=beliefState.getNbrOfGommes()*0.1;
-		//malus +=beliefState.getNbrOfSuperGommes();
-
-
-		/*Iterator<Position> it0  = tree0.iterator();
-		//Iterator<Position> it0 = tree0.descendingIterator();
-		while(it0.hasNext()){
-			Position p = it0.next();
-		}
-		System.out.println("Fin du fantome 0 pour ce mouv");*/
-		return beliefState.getScore()*10 + bonus-malus;
-	}
 
 	//Cas où lignes ==
 	private static boolean isWallRowBetween(Position pGhost, BeliefState beliefState) {
@@ -521,46 +270,5 @@ public class AI{
 		return false;
 
 	}
-	/*
-	public static boolean bfsSuperGomme(int[][] graph, char goal, BeliefState beliefState) {
-		int yPacman = beliefState.getPacmanPosition().getRow();
-		int xPacman = beliefState.getPacmanPosition().getColumn();
-		char start = beliefState.getMap(xPacman,yPacman);
-		int numNodes = 20*19;
-		boolean[] visited = new boolean[numNodes];
 
-
-		Queue<Character> queue = new LinkedList<>();
-		queue.add(start);
-		visited[start] = true;
-
-		while (!queue.isEmpty()) {
-			String currentNode = queue.poll();
-			if (currentNode.equals(goal)) {
-				return true;
-			}
-
-			for (int i = 0; i < numNodes; i++) {
-				if(beliefState.getMap(xPacman+1,yPacman+1)!= '#'  && !visited[beliefState.getMap(xPacman+1,yPacman+1)]){
-					queue.add(beliefState.getMap(xPacman+1,yPacman+1));
-					visited[beliefState.getMap(xPacman+1,yPacman+1)] = true;
-				}
-				if(beliefState.getMap(xPacman+1,yPacman)!='#'  && !visited[beliefState.getMap(xPacman+1,yPacman)]){
-					queue.add(beliefState.getMap(xPacman+1,yPacman));
-					visited[beliefState.getMap(xPacman+1,yPacman)] = true;
-				}
-				if(beliefState.getMap(xPacman,yPacman+1)!='#'  && !visited[beliefState.getMap(xPacman,yPacman+1)]){
-					queue.add(beliefState.getMap(xPacman,yPacman+1));
-					visited[beliefState.getMap(xPacman,yPacman+1)] = true;
-				}
-
-			}
-		}
-		return false;
-	}
-
-*/
-
-	//Un OU pour chaque mouvement
-	//Un ET pour que element de result
 }
